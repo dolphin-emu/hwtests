20d4bb18cc60cfc1ccd27b1b89576c68de5a99e0
Tony Wasserka <neobrainx@gmail.com>
2014-03-07 13:23:40 +0100

    Add abstract BitField class.

---
+++ source/BitField.h
@@ -0,0 +1,67 @@
+// Copyright 2013 Dolphin Emulator Project
+// Licensed under GPLv2
+// Refer to the license.txt file included.
+
+#include <assert.h>
+
+#pragma once
+
+// NOTE: Only works for sizeof(T)<=4 bytes
+// NOTE: Only(?) works for unsigned fields?
+template<u32 position, u32 bits, typename T=u32>
+struct BitField
+{
+private:
+	BitField(u32 val)
+	{
+		// This constructor just doesn't make any sense at all, so just assert out of it
+		assert(false);
+	}
+
+	BitField(const BitField& other)
+	{
+		// creates new storage => NOT what we want
+		assert(false);
+	}
+
+	BitField& operator = (const BitField& other)
+	{
+		// creates new storage => NOT what we want
+		assert(false);
+	}
+
+public:
+	BitField& operator = (u32 val)
+	{
+		storage = (storage & ~GetMask()) | ((val<<position) & GetMask());
+		return *this;
+	}
+
+	operator u32() const { return (storage & GetMask()) >> position; }
+
+	static T MaxVal()
+	{
+		// TODO
+		return (1<<bits)-1;
+	}
+
+private:
+	constexpr u32 GetMask()
+	{
+		return ((bits == 32) ? 0xFFFFFFFF : ((1 << bits)-1)) << position;
+	}
+
+	T storage;
+
+	friend class BitFieldWrapper;
+};
+
+/* Example:
+ */
+union SomeClass
+{
+	u32 hex;
+	BitField<0,7> first_seven_bits;
+	BitField<7,8> next_eight_bits;
+	// ...
+};
