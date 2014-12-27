a3d69a089a76b166d25acacbd488f3599d732ff1
Tillmann Karras <tilkax@gmail.com>
2014-12-27 21:19:29 +0100

    Segment registers: use templates instead of macros

--- cputest/segmentregisters.cpp
+++ cputest/segmentregisters.cpp
@@ -19,121 +19,128 @@ u32 sr_values[][2] =
 	{0x0F0FFFFF, 0x0F0FFFFF},
 };
 
-static void mtsrTest()
+template<u8 sr>
+inline static void DoMtsrTest()
+{
+	for (size_t i = 0; i < sizeof(sr_values) / sizeof(sr_values[0]); i++)
+	{
+		u32 input = sr_values[i][0];
+		u32 expected = sr_values[i][1];
+		u32 result = 0;
+		asm("mtsr %0, %1" :: "I" (sr), "r" (input));
+		asm("mfsr %0, %1" : "=r" (result) : "I" (sr));
+
+		DO_TEST(result == expected, "mtsr/mfsr(%u, 0x%08x):\n"
+		                            "\tgot      0x%08x\n"
+		                            "\texpected 0x%08x", sr, input, result, expected);
+	}
+}
+
+static void MtsrTest()
 {
 	START_TEST();
-#define MTSR_TEST(x) \
-	do \
-	{ \
-		for (size_t i = 0; i < sizeof(sr_values) / sizeof(sr_values[0]); i++) \
-		{ \
-			u32 input = sr_values[i][0]; \
-			u32 expected = sr_values[i][1]; \
-			u32 result = 0; \
-			asm("mtsr " #x ", %0" :: "r" (input)); \
-			asm("mfsr %0, " #x : "=r" (result)); \
-			\
-			DO_TEST(result == expected, "mtsr/mfsr(0x%08x):\n" \
-			                            "\tgot 0x%08x\n" \
-			                            "\texpected 0x%08x", input, result, expected); \
-		} \
-	} while(0)
-	MTSR_TEST(0);
-	MTSR_TEST(1);
-	MTSR_TEST(2);
-	MTSR_TEST(3);
-	MTSR_TEST(4);
-	MTSR_TEST(5);
-	MTSR_TEST(6);
-	MTSR_TEST(7);
-	MTSR_TEST(8);
-	MTSR_TEST(9);
-	MTSR_TEST(10);
-	MTSR_TEST(11);
-	MTSR_TEST(12);
-	MTSR_TEST(13);
-	MTSR_TEST(14);
-	MTSR_TEST(15);
+
+	DoMtsrTest<0>();
+	DoMtsrTest<1>();
+	DoMtsrTest<2>();
+	DoMtsrTest<3>();
+	DoMtsrTest<4>();
+	DoMtsrTest<5>();
+	DoMtsrTest<6>();
+	DoMtsrTest<7>();
+	DoMtsrTest<8>();
+	DoMtsrTest<9>();
+	DoMtsrTest<10>();
+	DoMtsrTest<11>();
+	DoMtsrTest<12>();
+	DoMtsrTest<13>();
+	DoMtsrTest<14>();
+	DoMtsrTest<15>();
 
 	END_TEST();
 }
 
-static void mtsrinTest()
+template<u8 sr>
+inline static void DoMtsrinTest()
+{
+	u32 rB = (u32)sr << (32 - 4);
+
+	for (size_t i = 0; i < sizeof(sr_values) / sizeof(sr_values[0]); i++)
+	{
+		u32 input = sr_values[i][0];
+		u32 expected = sr_values[i][1];
+		u32 result = 0;
+		asm("mtsrin %0, %1" :: "r" (input), "r" (rB));
+		asm("mfsr %0, %1" : "=r" (result) : "I" (sr));
+
+		DO_TEST(result == expected, "mtsrin(sr(%u), 0x%08x):\n"
+		                            "\tgot      0x%08x\n"
+		                            "\texpected 0x%08x", sr, input, result, expected);
+	}
+}
+
+static void MtsrinTest()
 {
 	START_TEST();
-#define MTSRIN_TEST(x) \
-	do \
-	{ \
-		for (size_t i = 0; i < sizeof(sr_values) / sizeof(sr_values[0]); i++) \
-		{ \
-			u32 input = sr_values[i][0]; \
-			u32 expected = sr_values[i][1]; \
-			u32 result = 0; \
-			u32 sr = x << 28; \
-			asm("mtsrin %0, %1" :: "r" (input) , "r" (sr)); \
-			asm("mfsr %0, " #x : "=r" (result)); \
-			\
-			DO_TEST(result == expected, "mtsrin(sr(%d)0x%08x):\n" \
-			                            "\tgot 0x%08x\n" \
-			                            "\texpected 0x%08x", x, input, result, expected); \
-		} \
-	} while(0)
-	MTSRIN_TEST(0);
-	MTSRIN_TEST(1);
-	MTSRIN_TEST(2);
-	MTSRIN_TEST(3);
-	MTSRIN_TEST(4);
-	MTSRIN_TEST(5);
-	MTSRIN_TEST(6);
-	MTSRIN_TEST(7);
-	MTSRIN_TEST(8);
-	MTSRIN_TEST(9);
-	MTSRIN_TEST(10);
-	MTSRIN_TEST(11);
-	MTSRIN_TEST(12);
-	MTSRIN_TEST(13);
-	MTSRIN_TEST(14);
-	MTSRIN_TEST(15);
+
+	DoMtsrinTest<0>();
+	DoMtsrinTest<1>();
+	DoMtsrinTest<2>();
+	DoMtsrinTest<3>();
+	DoMtsrinTest<4>();
+	DoMtsrinTest<5>();
+	DoMtsrinTest<6>();
+	DoMtsrinTest<7>();
+	DoMtsrinTest<8>();
+	DoMtsrinTest<9>();
+	DoMtsrinTest<10>();
+	DoMtsrinTest<11>();
+	DoMtsrinTest<12>();
+	DoMtsrinTest<13>();
+	DoMtsrinTest<14>();
+	DoMtsrinTest<15>();
 
 	END_TEST();
 }
 
-static void mfsrinTest()
+template<u8 sr>
+inline static void DoMfsrinTest()
+{
+	u32 rB = (u32)sr << (32 - 4);
+	for (size_t i = 0; i < sizeof(sr_values) / sizeof(sr_values[0]); i++)
+	{
+		u32 input = sr_values[i][0];
+		u32 expected = sr_values[i][1];
+		u32 result = 0;
+		asm("mtsr %0, %1" :: "I" (sr), "r" (input));
+		asm("mfsrin %0, %1" : "=r" (result) : "r" (rB));
+
+		DO_TEST(result == expected, "mfsrin(0x%08x, sr(%u)):\n"
+		                            "\tgot 0x%08x\n"
+		                            "\texpected 0x%08x", input, sr, result, expected);
+	}
+}
+
+static void MfsrinTest()
 {
 	START_TEST();
-#define MFSRIN_TEST(x) \
-	do \
-	{ \
-		for (size_t i = 0; i < sizeof(sr_values) / sizeof(sr_values[0]); i++) \
-		{ \
-			u32 input = sr_values[i][0]; \
-			u32 expected = sr_values[i][1]; \
-			u32 result = 0; \
-			u32 sr = x << 28; \
-			asm("mtsr " #x ", %0" :: "r" (input)); \
-			asm("mfsrin %0, %1" : "=r" (result) : "r" (sr)); \
-			\
-			DO_TEST(result == expected, "mfsrin(sr(%d)0x%08x):\n" \
-			                            "\tgot 0x%08x\n" \
-			                            "\texpected 0x%08x", x, input, result, expected); \
-		} \
-	} while(0)
-	MFSRIN_TEST(0);
-	MFSRIN_TEST(1);
-	MFSRIN_TEST(2);
-	MFSRIN_TEST(3);
-	MFSRIN_TEST(4);
-	MFSRIN_TEST(5);
-	MFSRIN_TEST(6);
-	MFSRIN_TEST(7);
-	MFSRIN_TEST(8);
-	MFSRIN_TEST(9);
-	MFSRIN_TEST(10);
-	MFSRIN_TEST(11);
-	MFSRIN_TEST(12);
-	MFSRIN_TEST(13);
-	MFSRIN_TEST(14);
-	MFSRIN_TEST(15);
+
+	DoMfsrinTest<0>();
+	DoMfsrinTest<1>();
+	DoMfsrinTest<2>();
+	DoMfsrinTest<3>();
+	DoMfsrinTest<4>();
+	DoMfsrinTest<5>();
+	DoMfsrinTest<6>();
+	DoMfsrinTest<7>();
+	DoMfsrinTest<8>();
+	DoMfsrinTest<9>();
+	DoMfsrinTest<10>();
+	DoMfsrinTest<11>();
+	DoMfsrinTest<12>();
+	DoMfsrinTest<13>();
+	DoMfsrinTest<14>();
+	DoMfsrinTest<15>();
 
 	END_TEST();
 }
@@ -141,9 +148,9 @@ int main()
 {
 	network_init();
 
-	mtsrTest();
-	mtsrinTest();
-	mfsrinTest();
+	MtsrTest();
+	MtsrinTest();
+	MfsrinTest();
 
 	network_printf("Shutting down...\n");
 	network_shutdown();
